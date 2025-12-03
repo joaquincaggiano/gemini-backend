dfc100994325081bd6b5f5ed599b6ed00f3e6d34
Joaquín Caggiano <joaquincaggiano@MacBook-Pro-de-Joaquin.local>
2025-12-03 19:18:08 +0100

    feat(): basic prompt stream

--- src/gemini/gemini.controller.ts
+++ src/gemini/gemini.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
 import { GeminiService } from './gemini.service';
+import type { Response } from 'express';
 import { BasicPromptDto } from './dtos/basic-prompt.dto';
 
 @Controller('gemini')
@@ -11,5 +12,22 @@ export class GeminiController {
     const response = await this.geminiService.basicPrompt(basicPromptDto);
 
     return response;
+  }
+
+  @Post('basic-prompt-stream')
+  async basicPromptStream(
+    @Body() basicPromptDto: BasicPromptDto,
+    @Res() res: Response,
+  ) {
+    const stream = await this.geminiService.basicPromptStream(basicPromptDto);
+
+    res.setHeader('Content-Type', 'text/plain');
+    res.status(HttpStatus.OK);
+
+    for await (const chunk of stream) {
+      res.write(chunk.text);
+    }
+
+    res.end();
   }
 }
--- src/gemini/gemini.service.ts
+++ src/gemini/gemini.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { BasicPromptDto } from './dtos/basic-prompt.dto';
 import { GoogleGenAI } from '@google/genai';
 import { basicPromptUseCase } from './use-cases/basic-prompt.use-case';
+import { basicPromptStreamUseCase } from './use-cases/basic-prompt-stream.use-case';
 
 @Injectable()
 export class GeminiService {
@@ -9,5 +10,9 @@ export class GeminiService {
 
   async basicPrompt(basicPromptDto: BasicPromptDto) {
     return basicPromptUseCase(this.ai, basicPromptDto);
+  }
+
+  async basicPromptStream(basicPromptDto: BasicPromptDto) {
+    return basicPromptStreamUseCase(this.ai, basicPromptDto);
   }
 }
