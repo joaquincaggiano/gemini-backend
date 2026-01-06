349b4bee25f8ebbc1cde5424385e604e1d685ffd
Joaquín Caggiano <joaquincaggiano@MacBook-Pro-de-Joaquin.local>
2026-01-06 21:19:42 +0100

    feat(): chat prompt dto and controller

--- src/gemini/gemini.controller.ts
+++ src/gemini/gemini.controller.ts
@@ -11,10 +11,30 @@ import { GeminiService } from './gemini.service';
 import type { Response } from 'express';
 import { BasicPromptDto } from './dtos/basic-prompt.dto';
 import { FilesInterceptor } from '@nestjs/platform-express';
+import { ChatPromptDto } from './dtos/chat-prompt.dto';
+import { GenerateContentResponse } from '@google/genai';
 
 @Controller('gemini')
 export class GeminiController {
   constructor(private readonly geminiService: GeminiService) {}
+
+  async outputStreamResponse(
+    res: Response,
+    stream: AsyncGenerator<GenerateContentResponse, any, any>,
+  ) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.status(HttpStatus.OK);
+
+    let resultText = '';
+    for await (const chunk of stream) {
+      const piece = chunk.text;
+      resultText += piece;
+      res.write(piece);
+    }
+
+    res.end();
+    return resultText;
+  }
 
   @Post('basic-prompt')
   async basicPrompt(@Body() basicPromptDto: BasicPromptDto) {
@@ -34,13 +54,22 @@ export class GeminiController {
 
     const stream = await this.geminiService.basicPromptStream(basicPromptDto);
 
-    res.setHeader('Content-Type', 'text/plain');
-    res.status(HttpStatus.OK);
+    await this.outputStreamResponse(res, stream);
+  }
 
-    for await (const chunk of stream) {
-      res.write(chunk.text);
-    }
+  @Post('chat-stream')
+  @UseInterceptors(FilesInterceptor('files'))
+  async chatStream(
+    @Body() chatPromptDto: ChatPromptDto,
+    @Res() res: Response,
+    @UploadedFiles() files: Express.Multer.File[],
+  ) {
+    chatPromptDto.files = files ?? [];
 
-    res.end();
+    const stream = await this.geminiService.basicPromptStream(chatPromptDto);
+
+    const data = await this.outputStreamResponse(res, stream);
+
+    console.log({ data });
   }
 }
