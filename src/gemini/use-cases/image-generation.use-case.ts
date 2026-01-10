2a57828d91f971aa7b3e56730595ac5e14025597
Joaquín Caggiano <joaquincaggiano@MacBook-Pro-de-Joaquin.local>
2026-01-10 13:45:25 +0100

    feat(): image generation use case wip

--- src/gemini/use-cases/image-generation.use-case.ts
+++ src/gemini/use-cases/image-generation.use-case.ts
@@ -1,6 +1,12 @@
-import { GoogleGenAI } from '@google/genai';
+import {
+  ContentListUnion,
+  createPartFromUri,
+  GoogleGenAI,
+  Modality,
+} from '@google/genai';
 import { geminiUploadFiles } from '../helpers/gemini-upload-files';
 import { ImageGenerationDto } from '../dtos/image-generation.dto';
+import { v4 as uuidv4 } from 'uuid';
 
 interface Options {
   model?: string;
@@ -19,12 +25,45 @@ export const imageGenerationUseCase = async (
 ): Promise<ImageGenerationResponse> => {
   const { prompt, files = [] } = imageGenerationDto;
 
+  const contents: ContentListUnion = [
+    {
+      text: prompt,
+    },
+  ];
+
   const uploadedFiles = await geminiUploadFiles(ai, files);
 
-  const {
-    model = 'gemini-2.5-flash',
-    systemInstruction = 'Responde únicamente en español, en formato markdown, Usa el sistema métrico decimal',
-  } = options ?? {};
+  uploadedFiles.forEach((file) => {
+    contents.push(createPartFromUri(file.uri ?? '', file.mimeType ?? ''));
+  });
+
+  const { model = 'gemini-2.5-flash-image' } = options ?? {};
+
+  const response = await ai.models.generateContent({
+    model,
+    contents,
+    config: {
+      responseModalities: [Modality.IMAGE, Modality.TEXT],
+    },
+  });
+
+  let imageUrl = '';
+  let text = '';
+  const imageId = uuidv4();
+
+  for (const part of response.candidates?.[0]?.content?.parts ?? []) {
+    if (part.text) {
+      text = part.text;
+      continue;
+    }
+
+    if (!part.inlineData) {
+      continue;
+    }
+
+    const imageData = part.inlineData.data!;
+    const buffer = Buffer.from(imageData, 'base64');
+  }
 
   return {
     imageUrl: '',
